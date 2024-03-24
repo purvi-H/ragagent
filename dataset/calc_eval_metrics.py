0308dfd61a82356540a992c0f769c0a9408c0ce6
purvi-h <s2024596@ed.ac.uk>
2024-03-24 19:04:22 +0000

    eval: perplexity calc

---
+++ dataset/calc_eval_metrics.py
@@ -0,0 +1,31 @@
+from evaluate import load
+
+def generate_gold_and_generated():
+    gold = []
+    generated = []
+    # Read the text file
+    with open("/Users/purviharwani/Desktop/ragagent/dataset/processed_baseline_results.txt", "r") as file:
+        lines = file.readlines()
+
+    # Process the lines to extract gold and generated pairs
+    i = 0
+    while i < len(lines):
+        if lines[i].startswith("Gold:"):
+            gold_text = lines[i][5:].strip()
+            generated_text = lines[i+1][11:].strip()
+            gold.append(gold_text)
+            generated.append(generated_text)
+            i += 2  # Move to the next pair
+        else:
+            i += 1  # Move to the next line
+    return gold, generated
+
+def calc_perplexity(generated): #generated = list of list
+    perplexity = load("perplexity", module_type="metric")
+    results = perplexity.compute(predictions=generated, model_id = "meta-llama/Llama-2-7b-chat-hf")
+    return results
+
+if __name__ == "__main__":
+    gold, generated = generate_gold_and_generated()
+    results = calc_perplexity(generated)
+    print(round(results["mean_perplexity"], 3))
